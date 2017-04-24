ecf185e09787c41c8b0db8fdb1b2349469f7f452
Abhi <a.kaulgud@gmail.com>
2017-04-24 12:07:20 -0400

    logic change
    logic change

--- player.py
+++ player.py
@@ -18,58 +18,14 @@ class Player(object):
             frame_score = 0
 
             # this is brutal but works, needs to be simplified
-
-            if frame < 10:
-                if result['strike']:
-                    frame_score = frame_score + 10
-                    if frame + 1 in self.results:
-                        if frame + 1 < 10:
-                            if self.results[frame + 1]['strike']:
-                                frame_score = frame_score + 10
-                                if frame + 2 in self.results:
-                                    if self.results[frame + 2]['strike']:
-                                        frame_score = frame_score + 10
-                                    else:
-                                        frame_score = frame_score + int(self.results[frame + 2]['roll_1'])
-                            elif self.results[frame + 1]['spare']:
-                                frame_score = frame_score + 10
-                            else:
-                                frame_score = frame_score + int(self.results[frame + 1]['roll_1']) \
-                                              + int(self.results[frame + 1]['roll_2'])
-                        else:
-                            roll_1 = 10 if self.results[frame + 1]['roll_1'].lower() == 'x' else int(self.results[frame + 1]['roll_1'])
-                            if self.results[frame + 1]['roll_2'].lower() == 'x':
-                                roll_2 = 10
-                            elif self.results[frame + 1]['roll_2'] == "/":
-                                roll_2 = 10 - int(self.results[frame + 1]['roll_1'])
-                            else:
-                                roll_2 = int(self.results[frame + 1]['roll_2'])
-
-                            frame_score = frame_score + roll_1 + roll_2
-
-                elif result['spare']:
-                    if frame + 1 in self.results:
-                        if self.results[frame + 1]['strike']:
-                            frame_score = 20
-                        else:
-                            frame_score = 10 + int(self.results[frame + 1]['roll_1'])
-                else:
-                    frame_score = frame_score + int(result['roll_1']) + int(result['roll_2'])
+            currentIndex = result['index']
+            if result['strike'] or result['spare']:
+                if len(self.rolls) - 1 >= currentIndex + 2:
+                    frame_score = self.rolls[currentIndex] + self.rolls[currentIndex + 1] + self.rolls[currentIndex + 2]
             else:
-                roll_1 = 10 if result['roll_1'].lower() == 'x' else int(result['roll_1'])
-                if result['roll_2'].lower() == 'x':
-                    roll_2 = 10
-                elif result['roll_2'] == "/":
-                    roll_2 = 10 - int(result['roll_1'])
-                else:
-                    roll_2 = int(result['roll_2'])
-
-                try:
-                    roll_3 = 10 if result['roll_3'].lower() == 'x' else int(result['roll_3'])
-                except:
-                    roll_3 = 0
-
-                frame_score = roll_1 + roll_2 + roll_3
+                frame_score = self.rolls[currentIndex] + self.rolls[currentIndex + 1]
+                if frame == 10 and len(self.rolls) - 1 >= currentIndex + 2:
+                    frame_score = frame_score + self.rolls[currentIndex + 2]
 
             running_total = running_total + frame_score
 
